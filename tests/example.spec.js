8117860a2275e1e815b589752851cd21ea7c7e80
Pilar <47372639+pilimartinez@users.noreply.github.com>
2025-12-05 14:35:09 -0300

    Revert "Add Checkly CLI setup"

--- tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test')
-
-test('example test - Checkly homepage', async ({ page }) => {
-  await page.goto('https://welcome.checklyhq.com')
-  const companyLogo = page.getByAltText(/logo/i)
-  await expect(companyLogo).toBeVisible()
-})
